18175481d71527e04d984396842b1ed79d69e347
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-26 10:53:51 +0000

    Add relation

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,4 @@
 class Food < ApplicationRecord
+  belongs_to :users, class_name: User, foreign_key: :user_id
+  has_many :recipes, :through => :recipes_foods
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+  belongs_to :users, class_name: User, foreign_key: :user_id
+  has_many :foods, :through => :recipes_foods
 end
