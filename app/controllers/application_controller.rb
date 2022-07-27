74bfd659412e1a3da6439e73bce5cd088777ab30
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-27 22:18:27 +0300

    Styling and routes added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password ])
+      end
 end
