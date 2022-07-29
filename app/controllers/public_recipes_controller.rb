ad94eb7b7471d96d362273f4a9f94ea339c14b94
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-29 16:44:07 +0000

    Remove commented code

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -4,12 +4,12 @@ class PublicRecipesController < ApplicationController
     # Should display a list of all public recipes order by newest
     # Should lead to recipes details
     @recipes = Recipe.includes(:user).where(public: true).where.not(user_id: current_user.id).order(created_at: :desc)
-    p @recipes
+
     return if @recipes.empty?
+
     @public_recipes = []
     @recipes.each do |recipe|
       recipe_food = RecipesFood.where(recipe_id: recipe.id)
-      p recipe_food
       next if recipe_food.empty?
 
       food_items = recipe_food[0].quantity
@@ -23,16 +23,7 @@ class PublicRecipesController < ApplicationController
         food_items: food_items,
         total_price: total_price,
       }
-      p public_recipe
       @public_recipes << public_recipe
     end
-
-    # @foods = RecipesFood.joins(:recipe, :food)
-    # @user = @recipes.user
-    # @total_price = 0
-    # @foods.each do |food|
-    #   f = Food.find(food.food_id)
-    #   @total_price += (f.price.to_i * food.quantity)
-    # end
   end
 end
