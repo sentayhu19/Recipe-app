e42b63f23c19c398ccd53a9e57f07e9a374d3898
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-29 11:50:22 +0300

    Stylelinter error fixed

--- test/models/shopping_list_test.rb
+++ test/models/shopping_list_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class ShoppingListTest < ActiveSupport::TestCase
   # test "the truth" do
--- test/system/shopping_lists_test.rb
+++ test/system/shopping_lists_test.rb
@@ -1,39 +1,39 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class ShoppingListsTest < ApplicationSystemTestCase
-  setup do
+  before do
     @shopping_list = shopping_lists(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit shopping_lists_url
-    assert_selector "h1", text: "Shopping lists"
+    assert_selector 'h1', text: 'Shopping lists'
   end
 
-  test "should create shopping list" do
+  test 'should create shopping list' do
     visit shopping_lists_url
-    click_on "New shopping list"
+    click_on 'New shopping list'
 
-    click_on "Create Shopping list"
+    click_on 'Create Shopping list'
 
-    assert_text "Shopping list was successfully created"
-    click_on "Back"
+    assert_text 'Shopping list was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Shopping list" do
+  test 'should update Shopping list' do
     visit shopping_list_url(@shopping_list)
-    click_on "Edit this shopping list", match: :first
+    click_on 'Edit this shopping list', match: :first
 
-    click_on "Update Shopping list"
+    click_on 'Update Shopping list'
 
-    assert_text "Shopping list was successfully updated"
-    click_on "Back"
+    assert_text 'Shopping list was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Shopping list" do
+  test 'should destroy Shopping list' do
     visit shopping_list_url(@shopping_list)
-    click_on "Destroy this shopping list", match: :first
+    click_on 'Destroy this shopping list', match: :first
 
-    assert_text "Shopping list was successfully destroyed"
+    assert_text 'Shopping list was successfully destroyed'
   end
 end
