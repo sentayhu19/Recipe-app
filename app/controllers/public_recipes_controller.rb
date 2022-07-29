acdc8f184da6c5d4159e3dc91e34faf6abbc8a73
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-29 13:05:37 +0300

    Fix the issue with public recipes public to true

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -3,7 +3,7 @@ class PublicRecipesController < ApplicationController
   def index
     # Should display a list of all public recipes order by newest
     # Should lead to recipes details
-    @recipes = Recipe.includes(:user).where(public: false).order(created_at: :desc)
+    @recipes = Recipe.includes(:user).where(public: true).order(created_at: :desc)
     @recipe_foods = RecipesFoods.includes(:food).where(recipe_id: @recipes.ids)
   end
 end
