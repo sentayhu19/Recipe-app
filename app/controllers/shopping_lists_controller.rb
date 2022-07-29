6a899acdb4afe7feaea5dfab3ed61abe2821e0eb
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-29 11:45:46 +0300

    Linters error fixed

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,19 +1,19 @@
 class ShoppingListsController < ApplicationController
-  before_action :set_shopping_list, only: %i[ show edit update destroy ]
+  before_action :set_shopping_list, only: %i[show edit update destroy]
 
   # GET /shopping_lists or /shopping_lists.json
   def index
     @recipe = Recipe.find_by(id: params[:recipe_id])
     @recipes_foods = []
-        @recipes_foods.each do |recipe_food|
-          next unless recipe_food.food_id == inventory_food.food_id
-          food = recipe_food
-          food.quantity -= inventory_food.quantity
-          @recipes_foods << food
-        end
-    
+    @recipes_foods.each do |recipe_food|
+      next unless recipe_food.food_id == inventory_food.food_id
+
+      food = recipe_food
+      food.quantity -= inventory_food.quantity
+      @recipes_foods << food
+    end
+
     @total_price = 0
     @recipes_foods.each { |item| @total_price += item.quantity * item.food.price }
   end
-
 end
