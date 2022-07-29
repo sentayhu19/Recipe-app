a27f0bc499391942d852a2be47a32206d98bb38a
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-29 12:57:32 +0000

    Fix public recipe issue

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -5,12 +5,13 @@ class PublicRecipesController < ApplicationController
     # Should lead to recipes details
     @recipes = Recipe.includes(:user).where(public: true).where.not(user_id: current_user.id).order(created_at: :desc)
     @foods = RecipesFood.joins(:recipe, :food)
-    @user = User.find(Food.find(@foods[0]['food_id'])['user_id'])
-    p @user
-    @total_price = 0
-    @foods.each do |food|
-      f = Food.find(food.food_id)
-      @total_price += (f.price.to_i * food.quantity)
+    unless @foods.empty?
+      @user = User.find(Food.find(@foods[0]['food_id'])['user_id'])
+      @total_price = 0
+      @foods.each do |food|
+        f = Food.find(food.food_id)
+        @total_price += (f.price.to_i * food.quantity)
+      end
     end
   end
 end
