766b3685ab80cece989bf413f0fa25bcf175bd64
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-29 11:33:04 +0300

    Shoping list functionality

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -4,5 +4,6 @@ class PublicRecipesController < ApplicationController
     # Should display a list of all public recipes order by newest
     # Should lead to recipes details
     @recipes = Recipe.includes(:user).where(public: false).order(created_at: :desc)
+    @recipe_foods = RecipesFoods.includes(:food).where(recipe_id: @recipes.ids)
   end
 end
--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -3,7 +3,17 @@ class ShoppingListsController < ApplicationController
 
   # GET /shopping_lists or /shopping_lists.json
   def index
-    @recipes = Recipe.includes(:recipeFoods).where(public: true)
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    @recipes_foods = []
+        @recipes_foods.each do |recipe_food|
+          next unless recipe_food.food_id == inventory_food.food_id
+          food = recipe_food
+          food.quantity -= inventory_food.quantity
+          @recipes_foods << food
+        end
+    
+    @total_price = 0
+    @recipes_foods.each { |item| @total_price += item.quantity * item.food.price }
   end
 
 end
