238cc836a61522108665f8b75c1c10acf43a39f0
Cédric Kossi <lionel.kossi@gmail.com>
2022-09-02 23:43:39 +0000

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  resources :shopping_lists
-  resources :foods
   devise_for :users
+  resources :shopping_lists
 
+  resources :foods, only: %i[index new create show destroy]
+  resources :users, only: %i[index new create show destroy]
   resources :recipes, only: %i[index new create show destroy] do
     resources :recipes_foods, only: %i[new create]
-  end
-
-  resources :users, only: %i[index show] do
-    resources :foods, only: %i[index new create show destroy]
-    resources :recipes, only: %i[index new create show destroy] do
-      resources :recipes_foods, only: %i[new create]
-    end
   end
 
   root 'users#index'
